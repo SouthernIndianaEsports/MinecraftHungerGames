704551e30087d31c1c5caabed002bfa4643f8d40
endoffile78 <endoffile78@yahoo.com>
2018-10-22 23:01:34 -0500

    Fix formatting and improve some of the code

--- src/me/Zachary_Peculier/SIEAHungerGames/MyPlayerListener.java
+++ src/me/Zachary_Peculier/SIEAHungerGames/MyPlayerListener.java
@@ -14,49 +14,57 @@ public class MyPlayerListener implements Listener
 {
     public static core plugin;
     int players;
+    List<String> alive = new ArrayList<String>();
+    List<String> dead = new ArrayList<String>();
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event)
     {
-	Player player = event.getEntity();
-	event.setDeathMessage(ChatColor.YELLOW + player.getName() + ChatColor.AQUA + " has been killed!");
-	players--;
+        Player player = event.getEntity();
+        String player_name = player.getName();
 
-	if (player.isDead())
-	{
-	    player.setGameMode(GameMode.SPECTATOR);
-	}
-	if (players == 1)
-	{
-	    Bukkit.broadcastMessage(ChatColor.RED + "GAME OVER! WE HAVE A WINNER!");
-	    
-	} else
-	{
-	    Bukkit.broadcastMessage(ChatColor.AQUA + "There are " + ChatColor.YELLOW + players + ChatColor.AQUA + " players remaining");
-	}
+        event.setDeathMessage(ChatColor.YELLOW + player_name + ChatColor.AQUA + " has been killed!");
+        players--;
+
+        player.setGameMode(GameMode.SPECTATOR);
+        alive.remove(player_name);
+        dead.add(player_name);
+
+        if (players == 1) {
+            Bukkit.broadcastMessage(ChatColor.RED + "GAME OVER! WE HAVE A WINNER! CONGRATULATIONS " + player_name + "!!!!");
+        } else {
+            Bukkit.broadcastMessage(ChatColor.AQUA + "There are " + ChatColor.YELLOW + players + ChatColor.AQUA + " players remaining");
+        }
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event)
     {
-	Player player = event.getPlayer();
-	event.setJoinMessage(
-		ChatColor.GREEN + player.getName() + ChatColor.RESET + " / " + ChatColor.DARK_GRAY + "has logged in!");
-
+        Player player = event.getPlayer();
+        event.setJoinMessage(ChatColor.GREEN + player.getName() + ChatColor.RESET + " / " + ChatColor.DARK_GRAY + "has logged in!");
+        alive.add(player.getName());
     }
 
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event)
     {
-	Player player = event.getPlayer();
-	event.setQuitMessage(
-		ChatColor.GREEN + player.getName() + ChatColor.RESET + " / " + ChatColor.DARK_GRAY + "has logged out!");
+        Player player = event.getPlayer();
+        String player_name = player.getName();
+
+        event.setQuitMessage(ChatColor.GREEN + player_name + ChatColor.RESET + " / " + ChatColor.DARK_GRAY + "has logged out!");
+
+        if (alive.contains(player_name)) {
+            alive.remove(player_name);
+        }
+
+        if (dead.contains(player_name)) {
+            dead.remove(player_name);
+        }
     }
 
     @SuppressWarnings("unused")
     public void InstantiateTribs(int tributes)
     {
-	players = tributes;
-
+        players = tributes;
     }
 }
--- src/me/Zachary_Peculier/SIEAHungerGames/core.java
+++ src/me/Zachary_Peculier/SIEAHungerGames/core.java
@@ -21,79 +21,62 @@ public class core extends JavaPlugin
     @Override
     public void onEnable()
     {
-
-	this.logger.info(pdFile.getName() + " " + pdFile.getVersion() + "has been activated!");
+        this.logger.info(pdFile.getName() + " " + pdFile.getVersion() + "has been activated!");
     }
 
     @Override
     public void onDisable()
     {
-	this.logger.info(pdFile.getName() + " " + pdFile.getVersion() + "has been deactivated!");
+        this.logger.info(pdFile.getName() + " " + pdFile.getVersion() + "has been deactivated!");
     }
 
     @SuppressWarnings("deprecation")
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
     {
-	Player player = (Player) sender;
-	if (commandLabel.equalsIgnoreCase("value"))
-	{
-	    if (args.length != 1)
-	    {
-		player.sendMessage(ChatColor.RED + "Usage: /value <players>");
-	    } else
-	    {
-		tributes = Integer.parseInt(args[0]);
-		mpl.InstantiateTribs(tributes);
+        Player player = (Player) sender;
+        if (commandLabel.equalsIgnoreCase("value")) {
+            if (args.length != 1) {
+                player.sendMessage(ChatColor.RED + "Usage: /value <players>");
+            } else {
+                tributes = Integer.parseInt(args[0]);
+                mpl.InstantiateTribs(tributes);
+            }
+        } else if (commandLabel.equalsIgnoreCase("start")) {
+            if (args.length != 1) {
+                player.sendMessage(ChatColor.RED + "Usage: /start <time in seconds>");
+            } else {
+                int time = Integer.parseInt(args[0]);
+                if (time > 0) {
+                    int seconds = time % 60;
+                    int minutes = time / 60;
+                    player.sendMessage(ChatColor.GREEN + "Timer for " + minutes + ":" + seconds + " started!");
+                    this.getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable(){
+                            int timer = time;
 
-	    }
-	}
-	if (commandLabel.equalsIgnoreCase("start"))
-	{
-	    if (args.length != 1)
-	    {
-		player.sendMessage(ChatColor.RED + "Usage: /start <time in seconds>");
-	    } else
-	    {
-		int time = Integer.parseInt(args[0]);
-		if (time > 0)
-		{
-		    int seconds = time % 60;
-		    int minutes = time / 60;
-		    player.sendMessage(ChatColor.GREEN + "Timer for " + minutes + ":" + seconds + " started!");
-		    this.getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable()
-		    {
-			int timer = time;
+                            public void run() {
+                                if (timer == -1) {
+                                    return;
+                                }
 
-			public void run()
-			{
-
-			    if (timer != -1)
-
-			    {
-				if (timer != 0)
-				{
-				    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[Server] " + timer);
-				    timer--;
-				} else
-				{
-				    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[Server] Go!");
-				    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE
-					    + "[Server] Good luck, and may the odds be ever in your favor");
-				    Bukkit.broadcastMessage(ChatColor.AQUA + "There are " + ChatColor.YELLOW + tributes
-					    + ChatColor.AQUA + " paricipants");
-				    timer--;
-				}
-			    }
-
-			}
-		    }, 0L, 20L);
-		    return true;
-		} else
-		{
-		    player.sendMessage(ChatColor.RED + "Please enter a whole number");
-		}
-	    }
-	}
-	return true;
+                                if (timer != 0) {
+                                    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[Server] " + timer);
+                                    timer--;
+                                } else {
+                                    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[Server] Go!");
+                                    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE
+                                                            + "[Server] Good luck, and may the odds be ever in your favor");
+                                    Bukkit.broadcastMessage(ChatColor.AQUA + "There are " + ChatColor.YELLOW + tributes
+                                                            + ChatColor.AQUA + " paricipants");
+                                    timer--;
+                                }
+                            }
+                        }, 0L, 20L);
+                    return true;
+                } else {
+                    player.sendMessage(ChatColor.RED + "Please enter a whole number");
+                }
+            }
+        }
+        return true;
     }
 }
