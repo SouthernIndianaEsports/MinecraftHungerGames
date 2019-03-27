28990d5c53624c5047177e378c18ce20a96304f7
ZacharyPeculier <zacharyjhardin@yahoo.com>
2019-03-26 20:39:40 -0500

    Disabled pvp in pregame

--- src/me/Zachary_Peculier/SIEAHungerGames/Listeners/PlayerListener.java
+++ src/me/Zachary_Peculier/SIEAHungerGames/Listeners/PlayerListener.java
@@ -8,6 +8,7 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -109,6 +110,15 @@ public class PlayerListener implements Listener
         else
         {
             alive.remove(player);
+        }
+    }
+
+    @EventHandler
+    public void onPvPDamage(EntityDamageEvent event)
+    {
+        if (!inProgress)
+        {
+            event.setCancelled(true);
         }
     }
 
