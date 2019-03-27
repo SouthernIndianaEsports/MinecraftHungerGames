11021b33b745bbd1bbec7f2347b4eb33c43c8cdf
endoffile78 <endoffile78@yahoo.com>
2019-03-26 21:47:01 -0500

    Actually fix merge

--- src/me/Zachary_Peculier/SIEAHungerGames/Commands/Who.java
+++ src/me/Zachary_Peculier/SIEAHungerGames/Commands/Who.java
@@ -23,32 +23,3 @@ public class Who implements CommandExecutor {
 	}
 
 }
-=======
-package me.Zachary_Peculier.SIEAHungerGames.Commands;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import me.Zachary_Peculier.SIEAHungerGames.Listeners.PlayerListener;
-
-public class Who implements CommandExecutor
-{
-
-    private final PlayerListener mpl;
-
-    public Who(final PlayerListener playerListener)
-    {
-        this.mpl = playerListener;
-    }
-
-    @Override
-    public final boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args)
-    {
-        Player player = (Player) sender;
-        mpl.listPlayers(player);
-        return true;
-    }
-
-}
